d95efecf39be20f706694664361add77c3b4c472
David O'Dey <david.odey@pearson.com>
2017-07-14 09:47:02 -0700

    initial commit

---
+++ _tasks/ftp.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sftp = require('gulp-sftp');
+
+gulp.task('ftp', function () {
+    return gulp.src('pattern-lab/public/**/*')
+        .pipe(sftp({
+            // host: '10.72.9.195',
+            // user: 'root',
+            // pass: 'hnapoi4716&&9012',
+            // remotePath: '/home/webroot/ux-framework'
+            host: '159.203.121.101',
+            user: 'root',
+            pass: 'j8912LKuuB$',
+            remotePath: '/home/webroot/pearson-glp-pl'
+        }));
+});
