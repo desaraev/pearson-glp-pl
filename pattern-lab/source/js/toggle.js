21d6fef2dd71879fb4605818cc11fbbc39fbef5b
David O'Dey <david.odey@pearson.com>
2017-07-31 09:40:48 -0700

    fixed basic accessibility issues

--- pattern-lab/source/js/toggle.js
+++ pattern-lab/source/js/toggle.js
@@ -1,17 +1,18 @@
 'use strict';
 (function() {
-    const toggle = document.querySelectorAll('toggle');
+    const toggle = document.querySelectorAll('.toggle');
     toggle.forEach(item => {
         const label = item.querySelector('label'),
-              input = item.querySelector('input');
-        label.onkeydown = (event) => {
-            if (event.keyCode ===  32) {
-                if (input.checked === false) {
-                    input.checked = true;
-                } else {
-                    input.checked = false;
-                }
-            }
+              input = item.querySelector('input'),
+              isChecked = input.getAttribute('aria-checked');
+
+        label.onclick = event => {
+            const target = event.currentTarget.previousSibling.previousSibling,
+                  isChecked = target.getAttribute('aria-checked');
+            if (isChecked === 'false')
+                target.setAttribute('aria-checked', 'true');
+            else
+                target.setAttribute('aria-checked', 'false');
         }
     })
 })();
