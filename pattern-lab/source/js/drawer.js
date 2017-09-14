4d15d1437b4bde44851cc281a16017320bf91b97
David O'Dey <david.odey@pearson.com>
2017-09-14 14:37:06 -0700

    notification drawer complete

--- pattern-lab/source/js/drawer.js
+++ pattern-lab/source/js/drawer.js
@@ -2,7 +2,8 @@
 
 (function() {
     const drawerButtons = document.querySelectorAll("button[data-drawer-id]"),
-          drawers = document.querySelectorAll('.drawer');
+          drawers = document.querySelectorAll('.drawer'),
+          actionButton = document.querySelector('.drawer-actions button');
 
     drawerButtons.forEach(button => {
         button.addEventListener('click', event => {
@@ -14,9 +15,38 @@
     });
 
     drawers.forEach(drawer => {
-        const button = drawer.querySelector('.title button');
+        const button = drawer.querySelector('.title > button'),
+              title = drawer.querySelector('.title .pe-title'),
+              backBtnContainer = drawer.querySelector('.title > div'),
+              backBtn = backBtnContainer.querySelector('button'),
+              archiveContent = drawer.querySelector('.archive'),
+              notificationContent = drawer.querySelector('.notifications');
+
         button.addEventListener('click', event => {
             event.currentTarget.parentNode.parentNode.classList.toggle('open');
-        })
+        });
+
+        actionButton.addEventListener('click', () => {
+            title.classList.add('hidden');
+            title.setAttribute('aria-hidden', 'true');
+            backBtnContainer.classList.remove('hidden');
+            backBtnContainer.setAttribute('aria-hidden', 'false');
+            notificationContent.classList.add('hidden');
+            notificationContent.setAttribute('aria-hidden', 'true');
+            archiveContent.classList.remove('hidden');
+            archiveContent.setAttribute('aria-hidden', 'false');
+
+        });
+
+        backBtn.addEventListener('click', () => {
+            title.classList.remove('hidden');
+            title.setAttribute('aria-hidden', 'false');
+            backBtnContainer.classList.add('hidden');
+            backBtnContainer.setAttribute('aria-hidden', 'true');
+            notificationContent.classList.remove('hidden');
+            notificationContent.setAttribute('aria-hidden', 'false');
+            archiveContent.classList.add('hidden');
+            archiveContent.setAttribute('aria-hidden', 'true');
+        });
     })
 })();
