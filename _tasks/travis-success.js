de7fd8b7ce23c7e1d743f7e314d9a7a731fa3678
David O'Dey <david.odey@pearson.com>
2017-12-01 15:02:03 -0700

    travis ftp

--- _tasks/travis-success.js
+++ _tasks/travis-success.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var sftp = require('gulp-sftp');
-
+var sshAuthSocket = process.env.SSH_AUTH_SOCK;
 gulp.task('travis-success', ['default'], function () {
     return gulp.src('pattern-lab/public/**/*')
         .pipe(sftp({
             host: '159.203.121.101',
-            user: 'thejimmy',
-            pass: 'jrp009SSPw&&',
+            user: 'davidodey',
+            agent: sshAuthSocket,
             remotePath: '/home/webroot/pearson-glp-pl'
         }));
 });
