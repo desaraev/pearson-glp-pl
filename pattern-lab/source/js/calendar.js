0f5a3ed0b297c68a585ad2c9d9f4e032670e25a7
David ODey <david.odey@pearson.com>
2017-12-19 10:08:31 -0700

    Calendar dynamic (#145)
    * changing creds for ftp -- WILL REMOVE ONCE SSH IN PLACE
    
    * removed console statements
    
    * minor fix on styling
    
    * added accesibility labels to calendar buttons
    
    * added git ignore
    
    * added aria-pressed to selected state and changed spans to H1 & H2 tags
    
    * changed tags from span to h1 and div to section

--- pattern-lab/source/js/calendar.js
+++ pattern-lab/source/js/calendar.js
@@ -38,7 +38,6 @@
 
             });
             calendar = [];
-
             weeks.forEach(week => {
                 if (i > 0 && week < weeks[i - 1]) {
 
@@ -92,7 +91,9 @@
                 dayClasses,
                 disabled,
                 elements = "",
-                formattedMonth;
+                formattedMonth,
+                isPressed = "false",
+                date;
 
                 dayList = [];
 
@@ -113,6 +114,7 @@
                     dayClasses = 'pe-link--btn pe-label neutral-two';
                     disabled = "";
                     formattedMonth = CalendarState.month + 1;
+                    date = day.format('dddd, MMMM Do YYYY');
 
                     if (!isCurrentMonth){
                         dayClasses += " muted";
@@ -121,6 +123,9 @@
 
                     if (isSelected){
                         dayClasses += " selected";
+                        isPressed = "true"
+                    } else {
+                        isPressed = "false"
                     }
 
                    if (isToday){
@@ -136,7 +141,7 @@
                         disabled = "disabled"
                     }
 
-                    return elements = '<button type="button" class="'+ dayClasses + '"'+disabled+'>'+day.format('D')+'</button>'
+                    return elements = '<button role="button" aria-pressed="'+isPressed+'" aria-label="'+date+'" class="'+ dayClasses + '"'+disabled+'>'+day.format('D')+'</button>'
                })
             }
         };
@@ -152,9 +157,10 @@
             month: GetCalendar.getState(calendarNumber).month,
             monthName: GetCalendar.getState(calendarNumber).monthName,
             calendar: GetCalendar.getState(calendarNumber).calendar,
-            selected: moment().format('DD-MM-YYYY')
+            selected: moment().format('DD-MM-YYYY'),
+            previousMonth: GetCalendar.getState(calendarNumber - 1).monthName
         };
-    
+
         renderDays = GetCalendar.render();
     
         const monthHTML = calendars[calendarNumber].querySelector('.month'),
@@ -185,8 +191,10 @@
             button.addEventListener('click', event => {
                 dayBtns.forEach((button) => {
                     button.classList.remove('selected');
+                    button.setAttribute('aria-pressed', 'false')
                 });
                 event.currentTarget.classList.add('selected');
+                event.currentTarget.setAttribute('aria-pressed', 'true')
             });
         }));
         weekArr.length = 0;
@@ -196,11 +204,27 @@
         renderCalendar("none", i);
     }
 
+    let returnMonthNames = (type, number = '') => {
+        if (type === "previous") {
+            return "Previous Month " + moment().month(CalendarState.month - number).format("MMMM")
+        } else if (type === "next") {
+            return "Next Month " + moment().month(CalendarState.month + number).format("MMMM")
+        }
+    };
+
     buttons.forEach((button,index) =>{
+        button[0].setAttribute('aria-label', returnMonthNames('previous', 1));
+        button[1].setAttribute('aria-label', returnMonthNames('next', 1));
+
         button[0].addEventListener('click', event => {
+            button[0].setAttribute('aria-label', returnMonthNames('previous', 2));
+            button[1].setAttribute('aria-label', returnMonthNames('next'));
             renderCalendar("prev", buttons.indexOf(button));
         });
+
         button[1].addEventListener('click', event => {
+            button[0].setAttribute('aria-label', returnMonthNames('previous'));
+            button[1].setAttribute('aria-label', returnMonthNames('next', 2));
             renderCalendar("next", buttons.indexOf(button));
         });   
     });
