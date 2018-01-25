c9dc7dc9619c9e175d946876f5690a8f71c63d0b
sarahvc <sarahvc@qq.com>
2018-01-25 14:38:09 -0500

    update modal with uxf, in progress

--- pattern-lab/source/js/modal.js
+++ pattern-lab/source/js/modal.js
@@ -2,8 +2,9 @@
 
 (function() {
     const modalButtons = document.querySelectorAll("button[data-modal-id]"),
-        overlay = document.querySelector('.overlay'),
-        closeButtons = document.querySelectorAll('.modal button');
+          overlay = document.querySelector('.overlay'),
+          actionButtons = document.querySelectorAll('.actions button'),
+          closeButtons = document.querySelectorAll('.close button');
 
     modalButtons.forEach(button => {
         button.addEventListener('click', event => {
@@ -12,14 +13,31 @@
             overlay.classList.remove('hidden');
             modalToOpen.classList.remove('hidden');
             modalToOpen.setAttribute('aria-hidden', 'false');
-        });
-    });
+        })
+    })
+
+    actionButtons.forEach(button => {
+        closeModal(button);
+    })
 
     closeButtons.forEach(button => {
+        closeModal(button);
+        // if (overlay.nextElementSibling.contains(button)){
+        //     overlay.addEventListener('click', event => {
+        //         button.parentNode.parentNode.classList.add('hidden');
+        //         button.parentNode.parentNode.setAttribute('aria-hidden', 'true');
+        //         overlay.classList.add('hidden');
+        //     })
+        // } 
+        // TODO assign id to each overlay
+    })
+
+    function closeModal(button) {
         button.addEventListener('click', event => {
             overlay.classList.add('hidden');
             event.currentTarget.parentNode.parentNode.classList.add('hidden');
             event.currentTarget.parentNode.parentNode.setAttribute('aria-hidden', 'true');
         })
-    })
+    }
+
 })();
