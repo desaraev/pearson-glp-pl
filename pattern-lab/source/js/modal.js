3a302a19bf639fc0258e8de1559d0fc0f45c8c64
sarahvc <sarahvc@qq.com>
2018-02-02 10:08:08 -0500

    update modal with the latest design

--- pattern-lab/source/js/modal.js
+++ pattern-lab/source/js/modal.js
@@ -2,17 +2,25 @@
 
 (function() {
     const modalButtons = document.querySelectorAll("button[data-modal-id]"),
-        overlay = document.querySelector('.overlay'),
-        actionButtons = document.querySelectorAll('.actions button'),
-        closeButtons = document.querySelectorAll('.close button');
+        scrollOverlays = document.querySelectorAll('.overlay-scroll'),
+        actionButtons = document.querySelectorAll('.modal-actions button'),
+        closeButtons = document.querySelectorAll('.modal-close');
 
     modalButtons.forEach(button => {
         button.addEventListener('click', event => {
             const modalId = event.currentTarget.getAttribute('data-modal-id'),
-                modalToOpen = document.querySelector('.modal[data-modal-open=' + modalId + ']');
-            overlay.classList.remove('hidden');
+                  modalToOpen = document.querySelector('.modal-body[data-modal-open=' + modalId + ']'),
+                  overlayToOpen = document.querySelector('.modal-overlay[data-overlay-open=' + modalId + ']');
+            overlayToOpen.classList.remove('hidden');
             modalToOpen.classList.remove('hidden');
             modalToOpen.setAttribute('aria-hidden', 'false');
+            if (overlayToOpen.classList.contains('content-scroll')){
+                const content = modalToOpen.querySelector('.modal-content');
+                if (content.scrollHeight > content.clientHeight) {
+                    content.classList.add('content-border');
+                    content.nextElementSibling.classList.add('actions-scroll-style');
+                }
+            }  
         })
     })
 
@@ -22,22 +30,29 @@
 
     closeButtons.forEach(button => {
         closeModal(button);
-        // if (overlay.nextElementSibling.contains(button)){
-        //     overlay.addEventListener('click', event => {
-        //         button.parentNode.parentNode.classList.add('hidden');
-        //         button.parentNode.parentNode.setAttribute('aria-hidden', 'true');
-        //         overlay.classList.add('hidden');
-        //     })
-        // } 
-        // TODO assign id to each overlay
+    })
+
+    scrollOverlays.forEach(overlay => {
+        overlay.addEventListener('click', event => {
+            const modalId = overlay.getAttribute('data-overlay-open');
+            overlay.classList.add('hidden');
+            closeModalBody(modalId); 
+        })
     })
 
     function closeModal(button) {
         button.addEventListener('click', event => {
-            overlay.classList.add('hidden');
-            event.currentTarget.parentNode.parentNode.classList.add('hidden');
-            event.currentTarget.parentNode.parentNode.setAttribute('aria-hidden', 'true');
+            const modalId = button.getAttribute('data-button-close'),
+                  overlayToClose = document.querySelector('.modal-overlay[data-overlay-open=' + modalId + ']');
+            overlayToClose.classList.add('hidden');
+            closeModalBody(modalId);
         })
+    }
+
+    function closeModalBody(id) {
+        const modalToClose = document.querySelector('.modal-body[data-modal-open=' + id + ']');
+        modalToClose.classList.add('hidden');
+        modalToClose.setAttribute('aria-hidden', 'true');
     }
 
 })();
